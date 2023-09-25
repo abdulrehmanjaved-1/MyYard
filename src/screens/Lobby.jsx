185df064b4ea0a1b994107d73ae85df17055d98f
abdulrehmanjaved-1 <abdulrehmanjaveds12@gmail.com>
2023-09-25 16:22:59 +0500

    done

--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -5,8 +5,8 @@ import ReactPlayer from "react-player";
 function LobbyScreen() {
   const [room, setRoom] = useState("");
   const [stream, setStream] = useState();
-  const [streamKey, setStreamKey] = useState(""); // Initialize with an empty string
-
+  const [selectedPlatforms, setSelectedPlatforms] = useState([]);
+  const [streamKeys, setStreamKeys] = useState({}); // Initialize as an empty object
   const socket = useSocket();
   const peer = new RTCPeerConnection({
     iceServers: [
@@ -60,33 +60,131 @@ function LobbyScreen() {
       console.error("Error getting user media:", error);
     }
   };
-  const handleStartStreaming = useCallback(async (e) => {
-    e.preventDefault();
-  
-    // Emit the "start-streaming" event to trigger streaming in the backend
-    socket.emit("start-streaming", streamKey); // Pass the stream key as an argument
-  }, [socket, streamKey]);
-  
+
+  const handleStartStreaming = useCallback(
+    async (e) => {
+      e.preventDefault();
+      console.log("hi",streamKeys)
+      // Emit the "start-streaming" event to trigger streaming in the backend
+      socket.emit("start-streaming", {
+        platforms: selectedPlatforms,
+        streamKeys: streamKeys,
+      });
+    },
+    [socket, selectedPlatforms, streamKeys]
+  );
+
+  // Function to update the stream keys when the user selects a platform
+  const handleStreamKeyChange = (platform, key) => {
+    setStreamKeys((prevStreamKeys) => ({
+      ...prevStreamKeys,
+      [platform]: key,
+    }));
+  };
 
   return (
     <div>
       <h1>Lobby Screen</h1>
 
       <form onSubmit={handleStartStreaming}>
-        {/* <label htmlFor="room">Room No:</label>
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        /> */}
-        <h3>Enter your Stream Key</h3>
-        <input
-          type="text"
-          id="streamKey"
-          value={streamKey}
-          onChange={(e) => setStreamKey(e.target.value)}
-        />
+        <h3>Select platforms to stream to:</h3>
+        <label>
+          <input
+            type="checkbox"
+            value="youtube"
+            onChange={(e) => {
+              if (e.target.checked) {
+                setSelectedPlatforms((prevPlatforms) => [
+                  ...prevPlatforms,
+                  "youtube",
+                ]);
+              } else {
+                setSelectedPlatforms((prevPlatforms) =>
+                  prevPlatforms.filter((platform) => platform !== "youtube")
+                );
+                // Remove the stream key when platform is deselected
+                handleStreamKeyChange("youtube", "");
+              }
+            }}
+          />
+          YouTube
+          {/* Input for entering the stream key for YouTube */}
+          {selectedPlatforms.includes("youtube") && (
+            <input
+              type="text"
+              placeholder="YouTube Stream Key"
+              value={streamKeys.youtube}
+              onChange={(e) =>
+                handleStreamKeyChange("youtube", e.target.value)
+              }
+            />
+          )}
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="facebook"
+            onChange={(e) => {
+              if (e.target.checked) {
+                setSelectedPlatforms((prevPlatforms) => [
+                  ...prevPlatforms,
+                  "facebook",
+                ]);
+              } else {
+                setSelectedPlatforms((prevPlatforms) =>
+                  prevPlatforms.filter((platform) => platform !== "facebook")
+                );
+                // Remove the stream key when platform is deselected
+                handleStreamKeyChange("facebook", "");
+              }
+            }}
+          />
+          Facebook
+          {/* Input for entering the stream key for YouTube */}
+          {selectedPlatforms.includes("facebook") && (
+            <input
+              type="text"
+              placeholder="Facebook Stream Key"
+              value={streamKeys.facebook}
+              onChange={(e) =>
+                handleStreamKeyChange("facebook", e.target.value)
+              }
+            />
+          )}
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="instagram"
+            onChange={(e) => {
+              if (e.target.checked) {
+                setSelectedPlatforms((prevPlatforms) => [
+                  ...prevPlatforms,
+                  "instagram",
+                ]);
+              } else {
+                setSelectedPlatforms((prevPlatforms) =>
+                  prevPlatforms.filter((platform) => platform !== "instagram")
+                );
+                // Remove the stream key when platform is deselected
+                handleStreamKeyChange("instagram", "");
+              }
+            }}
+          />
+          Instagram
+          {/* Input for entering the stream key for YouTube */}
+          {selectedPlatforms.includes("instagram") && (
+            <input
+              type="text"
+              placeholder="Instagram Stream Key"
+              value={streamKeys.instagram}
+              onChange={(e) =>
+                handleStreamKeyChange("instagram", e.target.value)
+              }
+            />
+          )}
+        </label>
+        {/* Add similar checkbox inputs and input fields for other platforms (e.g., Facebook, Instagram) */}
         <button>Start Streaming</button>
         {stream && (
           <>
